14dce5c13808fb92bc3671a1c1bfc52bfc490e72
silofox <silofox@uisn.co.kr>
2024-07-02 14:30:05 +0900

    cudnn setting finished and test compelete

--- tests/subtests/compatability_test.py
+++ tests/subtests/compatability_test.py
@@ -1,11 +1,17 @@
 from tensorflow.python.client import device_lib
+import tensorflow as tf
 
 def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
+def get_available_gpus_count():
+    physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    return len(physical_devices)
+
 if __name__ == "__main__":
     try : 
         print(get_available_gpus())
+        print("Number of GPUs: ", get_available_gpus_count())
     except Exception as e:
         print(e)
