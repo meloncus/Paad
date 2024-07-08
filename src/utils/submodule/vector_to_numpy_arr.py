ba5b1daf907f2286811c2b0731a05ef4efa56161
silofox <silofox@uisn.co.kr>
2024-07-08 13:02:36 +0900

    add submodule for utils and add vector to numpy arr.py in sub

---
+++ src/utils/submodule/vector_to_numpy_arr.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tqdm
+
+def vector_to_numpy_arr (vector_arr) :
+    '''
+    convert list of vectors to numpy array
+
+    input
+    vector_arr : list of numpy.array
+        list of vectors
+
+    output
+    dataset : numpy.array
+        numpy array of vectors
+    '''
+    len_list = len(vector_arr)
+
+    for idx in tqdm(range(len_list)) :
+        vector_list = vector_arr[idx]
+
+        if idx == 0 :
+            dataset = np.zeros((len_list, vector_list.shape[0], vector_list.shape[1]), float)
+        
+        dataset[idx, :, :] = vector_list
+
+    return dataset
