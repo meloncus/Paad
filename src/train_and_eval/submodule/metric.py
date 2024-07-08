b65fa7aba4cf978c7d6f2a7295f7abc84e4ce438
silofox <silofox@uisn.co.kr>
2024-07-08 10:59:09 +0900

    add calculate statistics module in metric.py

--- src/train_and_eval/submodule/metric.py
+++ src/train_and_eval/submodule/metric.py
@@ -1,4 +1,39 @@
+from sklearn import metrics
+import pandas as pd
 import numpy as np
+
+def calculate_stats (predictions, labels, machine, feat_label, model = "AE") :
+    '''
+    calculate auc, precision, recall, f1 for abnormal
+
+    input
+    predictions : numpy.array, predicted labels
+    labels : numpy.array, true labels
+    machine : string, machine name
+    feat_label : string, feature label ?
+    model : string, model name
+
+    output
+    df : pandas.DataFrame, calculated statistics
+    '''
+    auc_score = metrics.roc_auc_score(labels, predictions)
+    precision = metrics.precision_score(labels, predictions)
+    recall = metrics.recall_score(labels, predictions)
+    f1_for_abnormal = metrics.f1_score(labels, predictions, pos_label = -1, average = "binary")
+
+    results = {
+        "Model" : [model],
+        "Machine" : [machine],
+        "Feature_label" : [feat_label],
+        "AUC" : [auc_score],
+        "Precision" : [precision],
+        "Recall" : [recall],
+        "F1 for Abnormal" : [f1_for_abnormal],
+    }
+
+    df = pd.DataFrame(results)
+
+    return df
 
 def get_mse(source, target):
     '''
